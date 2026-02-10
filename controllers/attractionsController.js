b86a339e3f3e51a5284580c185f318f15899ede3
JLKohl <121449936+JLKohl@users.noreply.github.com>
2026-02-10 10:47:22 -0600

    attraction controllers complete and test endpoint set up

--- controllers/attractionsController.js
+++ controllers/attractionsController.js
@@ -5,8 +5,8 @@ const ObjectId = require('mongodb').ObjectId;
 const getAllAttractions = async (req, res, next) => {
   try {
     const db = mongodb.getDb().db('disney_planner');
-    const result = await db.collection('attractions').find().toArray();
-    res.status(200).json(result);
+    const response = await db.collection('attractions').find().toArray();
+    res.status(200).json(response);
   } catch (err) {
     res.status(500).json({ error: 'Something went wrong' });
   }
@@ -14,15 +14,15 @@ const getAllAttractions = async (req, res, next) => {
 };
 
 const getAttractionById = async (req, res) => {
-//   try {
-//     const db = require('../db/connect').getDb().db('Test');
-//     const id = req.params.id; // gets the value from the URL
-//     const contact = await db.collection('Contacts').findOne({ _id: new ObjectId(id) });
-//     if (!contact) return res.status(404).json({ error: 'Contact not found' });
-//     res.status(200).json(contact);
-//   } catch (err) {
-//     res.status(500).json({ error: 'Something went wrong' });
-//   }
+  try {
+    const db = require('../db/connect').getDb().db('disney_planner');
+    const id = req.params.id; // gets the value from the URL
+    const attraction = await db.collection('attractions').findOne({ _id: new ObjectId(id) });
+    if (!attraction) return res.status(404).json({ error: 'attraction not found' });
+    res.status(200).json(attraction);
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 };
 
 const newAttraction = async (req, res) => {
@@ -47,34 +47,36 @@ const newAttraction = async (req, res) => {
 
 const editAttraction = async (req, res) => {
 
-//   console.log('EDIT CONTACT HIT');
-//   console.log('ID:', req.params.id);
-//   console.log('BODY:', req.body);
+  console.log('EDIT attraction HIT');
+  console.log('ID:', req.params.id);
+  console.log('BODY:', req.body);
 
-//   const contactId = req.params.id;
-//   const updates = req.body;
+  const attractionId = req.params.id;
+  const updates = req.body;
 
-//   const response = await mongodb.getDb().db().collection('Contacts').updateOne({ _id: new ObjectId(contactId) },
-//   { $set: updates });
+  const response = await mongodb.getDb().db().collection('attractions').updateOne({ _id: new ObjectId(attractionId) },
+  { $set: updates });
 
-//   if (response.modifiedCount > 0) {
-//     res.status(204).send();
-//   } else {
-//     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
-//   }
+  if (response.matchedCount === 0) {
+    res.status(404).json({ message: 'Attraction not found' });
+  } else {
+    res.status(204).send();
+  }
+
+  res.status(200).json(response.value);
 }
 
 const deleteAttraction = async (req, res) => {
   
-//   const contactId = req.params.id;
+  const attractionId = req.params.id;
 
-//   const response = await mongodb.getDb().db().collection('Contacts').deleteOne({ _id: new ObjectId(contactId) });
+  const response = await mongodb.getDb().db().collection('attractions').deleteOne({ _id: new ObjectId(attractionId) });
 
-//   if (response.acknowledged) {
-//     res.status(200).json(response);
-//   } else {
-//     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
-//   }
+  if (response.acknowledged) {
+    res.status(200).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the attraction.');
+  }
 }
 
 
